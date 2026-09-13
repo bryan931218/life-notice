49a22424b54c3c454a95ed429e0a2e58d06b973c
bryan931218 <bryan931218@gmail.com>
2026-09-13 15:14:30 +0800

    fix: import listener component for rebind

--- modules/notice-listener/android/src/main/java/expo/modules/noticelistener/LifeNoticeListenerService.kt
+++ modules/notice-listener/android/src/main/java/expo/modules/noticelistener/LifeNoticeListenerService.kt
@@ -5,6 +5,7 @@ import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.app.RemoteInput
+import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.os.Build
